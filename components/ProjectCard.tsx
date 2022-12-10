e59a03d98cc73b4ae35f591f6bb138afe0334c05
JayDang <tonydang0124@gmail.com>
2022-12-10 00:09:48 -0800

    fix links

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,15 +1,21 @@
+import Link from 'next/link';
 import React from 'react';
 import Icon from './Icon';
 
-const ProjectCard = (props: { title: string; icon: string; children: React.ReactNode }) => {
+const ProjectCard = (props: {
+  title: string;
+  icon: string;
+  children: React.ReactNode;
+  href: string;
+}) => {
   return (
     <div className="w-full md:w-1/2 lg:w-1/3 pb-4 md:pr-4 cursor-pointer">
       <div className="card rounded-lg bg-base-100 shadow-xl border-0 h-56 overflow-hidden pb-2 hover:bg-base-300">
-        <div className="card-body">
+        <Link href={props.href} className="card-body">
           <Icon path={props.icon} />
           <h2 className="card-title">{props.title}</h2>
           <p className="text-gray-400">{props.children}</p>
-        </div>
+        </Link>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { NextPage } from 'next';
 import Link from 'next/link';
 import ProjectCard from '../components/ProjectCard';
 import Icon from '../components/Icon';
+import router from 'next/router';
 const Page: NextPage = () => {
   return (
     <div className="mx-auto max-w-6xl mt-12">
@@ -15,32 +16,35 @@ const Page: NextPage = () => {
       <div className="flex flex-wrap mt-6">
         <div className="w-full md:w-1/2 lg:w-1/3 pb-4 md:pr-4 cursor-pointer">
           <div className="card rounded-lg  h-56 overflow-hidden pb-2 hover:bg-base-300 bg-base-100 e">
-            <div className="card-body items-center text-center  mt-12">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round">
-                <circle cx="12" cy="12" r="10"></circle>
-                <line x1="12" y1="8" x2="12" y2="16"></line>
-                <line x1="8" y1="12" x2="16" y2="12"></line>
-              </svg>
-              <h2 className="font-bold card-title">Create New Project</h2>
-            </div>
+            <Link href="/projects/DEMO?tool=swot">
+              <div className="card-body items-center text-center  mt-12">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round">
+                  <circle cx="12" cy="12" r="10"></circle>
+                  <line x1="12" y1="8" x2="12" y2="16"></line>
+                  <line x1="8" y1="12" x2="16" y2="12"></line>
+                </svg>
+                <h2 className="font-bold card-title">Create New Project</h2>
+              </div>
+            </Link>
           </div>
         </div>
-
         <ProjectCard
+          href="/projects/DEMO?tool=swot"
           title={'SWOT Analysis'}
           icon={'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'}>
           Jump into a whole SWOT analysis in 5 seconds — all we need is the name of the company.
         </ProjectCard>
         <ProjectCard
+          href="/projects/DEMO?tool=business-overview"
           title={'Business Overview'}
           icon={
             'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z'
@@ -48,6 +52,7 @@ const Page: NextPage = () => {
           Learn about the business with a detailed overview.
         </ProjectCard>
         <ProjectCard
+          href="/projects/DEMO?tool=industry-report"
           title={'Industry Report'}
           icon={'M21.21 15.89A10 10 0 1 1 8 2.83 M22 12A10 10 0 0 0 12 2v10z'}>
           Get a detailed analysis of a certain industry, within seconds.
